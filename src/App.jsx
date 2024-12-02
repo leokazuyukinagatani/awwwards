0c50ece1580b7d3e46da37e062c3c9eed78669cd
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2024-12-02 10:51:07 -0300

    config: tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function App() {
     return (
-        <div>App</div>
+        <main>
+            <h1 className="text-5xl text-red-500 font-bold">Welcome to Awwwards</h1>
+        </main>
     )
 }
 
