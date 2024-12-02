74b02c18cf16fa3da57266dce0ec3854fc6a0b2e
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2024-12-02 11:05:02 -0300

    config: fonts and colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
     return (
         <main>
-            <h1 className="text-5xl text-red-500 font-bold">Welcome to Awwwards</h1>
+            <h1 className="text-5xl text-violet-300 font-bold">Welcome to Awwwards</h1>
         </main>
     )
 }
